bc2fde47805dabcb447b3ebd32c187c9125b76ad
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2023-02-11 22:59:02 +0530

    local storage

--- booklist/src/context/BookContext.js
+++ booklist/src/context/BookContext.js
@@ -1,10 +1,17 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer,[]);
+  const [books, dispatch] = useReducer(bookReducer,[], () => {
+    const local = localStorage.getItem('books');
+    return local ? JSON.parse(local) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books));
+  },[books])
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
